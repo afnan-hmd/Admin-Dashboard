19582fcbca199395c9a127ed3e48c13f1a49acc9
afnan-hmd <91302955+afnan-hmd@users.noreply.github.com>
2024-11-17 13:33:36 +0400

    Update fetch url

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let cardData = [];
 
     function fetchData() {
-        return fetch('/assets/data/cardInfo.json')
+        return fetch('https://afnan-hmd.github.io/Admin-Dashboard/assets/data/cardInfo.json')
         .then(res => {
             if(!res.ok) {
                 throw new Error('fetch response error')
